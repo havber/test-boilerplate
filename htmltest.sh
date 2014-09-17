1a652aac593318518d953af1ab6da97be2b83006
havber <havber@gmail.com>
2014-09-17 14:40:14 +0200

    Added shortcuts fro running tests

---
+++ htmltest.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./node_modules/mocha-casperjs/bin/mocha-casperjs test-html/*.js
--- test-html/html.js
+++ test-html/html.js
@@ -2,12 +2,12 @@ var assert = require('chai').assert;
 
 describe('Frontpage', function() {
     before(function() {
-        casper.start('http://localhost:8000')
+        casper.start('http://localhost:8000');
     });
 
     it('should have correct title', function() {
         casper.then(function() {
-            assert.equal('CasperJS test', this.getTitle());
+            assert.equal(this.getTitle(), 'CasperJS Test');
         });
     });
 });
